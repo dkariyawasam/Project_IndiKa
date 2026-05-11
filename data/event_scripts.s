fc07aa25bdac80c274c2208bca89a54258258faf
Dee Kariyawasam <dkariyawasam@icloud.com>
2026-05-11 09:35:24 +0100

    removed unsued hoenn layouts and removed trainer tips

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1335,7 +1335,6 @@ Text_TestMsg::
 
 	.include "data/scripts/fame_checker.inc"
 	.include "data/text/fame_checker.inc"
-	.include "data/text/sign_lady.inc"
 	.include "data/text/trainer_card.inc"
 	.include "data/scripts/trainer_card.inc"
 	.include "data/text/help_system.inc"
