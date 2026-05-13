13e4c98335337616ec60ddf1a9efc19a73ca9304
Dee Kariyawasam <dkariyawasam@icloud.com>
2026-05-13 16:55:47 +0100

    added cerulean gym guide sequence

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -189,7 +189,7 @@
 // Unused?
 #define FLAG_HIDE_PEWTER_CITY_GYM_BROCK               0x0AF
 #define FLAG_HIDE_MT_MOON_BROCK                       0x0B0
-#define FLAG_0x0B1               0x0B1
+#define FLAG_HIDE_CERULEAN_CITY_GYM_MISTY             0x0B1
 #define FLAG_0x0B2               0x0B2
 #define FLAG_0x0B3               0x0B3
 #define FLAG_0x0B4               0x0B4
