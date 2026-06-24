8ed16abf01130dca09f0c9fc812f08e6e9672fec
Dee Kariyawasam <dkariyawasam@icloud.com>
2026-06-24 19:36:54 +0100

    added ambipom menu icon

--- spritesheet_rules.mk
+++ spritesheet_rules.mk
@@ -421,6 +421,9 @@ $(OBJEVENTGFXDIR)/pokemon/slowpoke.4bpp: %.4bpp: %.png
 $(OBJEVENTGFXDIR)/pokemon/spearow.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 2
 
+$(OBJEVENTGFXDIR)/pokemon/tangrowth.4bpp: %.4bpp: %.png
+	$(GFX) $< $@ -mwidth 4 -mheight 4
+
 $(OBJEVENTGFXDIR)/pokemon/voltorb.4bpp: %.4bpp: %.png
 	$(GFX) $< $@ -mwidth 2 -mheight 2
 
